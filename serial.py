5f153bb9fa6dcbae8437864c89d549ecfc566065
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-04-18 15:34:05 -0700

    serial python

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+
+    def __init__(self, start):
+        """Accept a start value and assign a counter variable to the start value"""
+        self.start = start
+        self.counter = start
+
+    def generate(self):
+        """Increment the value of counter every time it is called and return the
+        counter less one"""
+
+        self.counter += 1
+        return self.counter - 1
+
+    def reset(self):
+        """When called, reset the counter back to the start value"""
+        self.counter = self.start
