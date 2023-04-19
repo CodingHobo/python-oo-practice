a6ed0f79b01f1b961c6f25ed5c91889fe72d8ccc
Tanya Shylock <tourtanya@gmail.com>
2023-04-18 19:11:56 -0600

    add veggies, finish wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -12,14 +12,14 @@ class WordFinder:
     def __init__(self, file_of_words):
         """Open the file and read it ; print the number of words read and
         convert the file to a list of words"""
-        self.word_file = open(file_of_words)
+        word_file = open(file_of_words)
 
-        self.list_of_words = self.convert_word_file_to_list(self.word_file)
+        self.list_of_words = self.convert_word_file_to_list(word_file)
 
         print(f"{len(self.list_of_words)} words read")
 
     def convert_word_file_to_list(self, word_file):
-        """Convert the self.word_file to list of words with whitespace and line
+        """Convert word_file to list of words with whitespace and line
         breaks removed"""
         return [word.strip() for word in word_file]
 
@@ -31,13 +31,10 @@ class SpecialWordFinder(WordFinder):
     """Accept a file of words and return a word that is not a space or
     in a #comment"""
 
-    def __init__(self, file_of_words):
-        """Inherit the initialized variables from the parent class"""
-        super().__init__(file_of_words)
+    def convert_word_file_to_list(self, word_file):
+        """strip line breaks and whitespace, filter words preceded by #"""
+        return [word for word in super().convert_word_file_to_list(word_file) if not (word.startswith("#") or word == "")]
 
-    def special_word():
-        
-        "insert method to strip symbols"super().find_random_words()
 
 
 
